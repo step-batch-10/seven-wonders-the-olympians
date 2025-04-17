7207ee536186f059a7f41c48eea9d39cb64ea24b
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2025-04-17 10:45:24 +0530

    [#32] | join game
    added functionlaty of login and add waiting room

---
+++ public/scripts/waiting_room.js
@@ -0,0 +1,22 @@
+const userName = async () => {
+  const res = await fetch("/user/name");
+  return `${await res.text()}!`;
+};
+
+const data = async () => {
+  const res = await fetch("/user/playerReady");
+  if (res.redirected) {
+    globalThis.location.href = res.url;
+  }
+};
+
+const displayUserName = async () => {
+  const name = await userName();
+  const location = document.querySelector(".name");
+  location.innerText = `${location.innerText}  ${name}`;
+  return setInterval(() => {
+    data();
+  }, 1000);
+};
+
+globalThis.onload = displayUserName;
