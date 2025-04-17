4d704869cfc69c2ca1a10c506925005a6cbc346c
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2025-04-17 17:09:40 +0530

    [#32] | join game
    corrected the routes on client side

--- public/scripts/waiting_room.js
+++ public/scripts/waiting_room.js
@@ -1,22 +1,22 @@
-// const userName = async () => {
-//   const res = await fetch("/user/name");
-//   return `${await res.text()}!`;
-// };
+const userName = async () => {
+  const res = await fetch("/auth/name");
+  return `${await res.text()}!`;
+};
 
-// const data = async () => {
-//   const res = await fetch("/user/playerReady");
-//   if (res.redirected) {
-//     globalThis.location.href = res.url;
-//   }
-// };
+const data = async () => {
+  const res = await fetch("/game/status");
+  if (res.redirected) {
+    globalThis.location.href = res.url;
+  }
+};
 
-// const displayUserName = async () => {
-//   const name = await userName();
-//   const location = document.querySelector(".name");
-//   location.innerText = `${location.innerText}  ${name}`;
-//   return setInterval(() => {
-//     data();
-//   }, 1000);
-// };
+const displayUserName = async () => {
+  const name = await userName();
+  const location = document.querySelector(".name");
+  location.innerText = `${location.innerText}  ${name}`;
+  return setInterval(() => {
+    data();
+  }, 1000);
+};
 
-// globalThis.onload = displayUserName;
+globalThis.onload = displayUserName;
