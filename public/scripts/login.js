1fb65dae4f866f99d9fa51156c8ad18fbfafc4a8
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2025-04-17 10:58:24 +0530

    [#32] join game
    added script file and communication with server

---
+++ public/scripts/login.js
@@ -0,0 +1,22 @@
+const login = async (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const name = data.get("username");
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ name }),
+  });
+  if (res.redirected) {
+    globalThis.location.href = res.url;
+  }
+};
+
+const main = () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", login);
+};
+globalThis.onload = main;
