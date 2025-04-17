ac77afb43462998f342fd4dc2bf239d9047ec83d
AkashKumar <124288637+Akash-2006@users.noreply.github.com>
2025-04-17 10:54:19 +0530

    [#32] | join game
    added functionality for routes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,38 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
 import type { Context } from "hono";
-
-const createApp = (): Hono => {
+import {
+  auth,
+  fetchUserName,
+  playerReady,
+  registerUser,
+} from "./routes/player_route.ts";
+import { getCookie, setCookie } from "hono/cookie";
+import { User } from "../main.ts";
+const createApp = (
+  users: { [key: string]: User },
+  waitQueue: string[],
+): Hono => {
   const app = new Hono();
+  app.use("*", async (ctx: Context, nxt) => {
+    ctx.getCookie = getCookie;
+    ctx.setCookie = setCookie;
+    ctx.users = users;
+    ctx.waitQueue = waitQueue;
+    ctx.user = ctx.getCookie(ctx, "name");
+    await nxt();
+  });
 
+  app.post("/login", registerUser);
+  app.use("/user/*", auth);
+  app.get("/user/playerReady", playerReady);
+  app.get("/user/name", fetchUserName);
   app
     .get("/", serveStatic({ path: "public/index.html" }))
     .get("/test", (ctx: Context) => {
       return ctx.text("Welcome to 7 wonders!");
     })
-    .use(serveStatic({root: "public/"}));
+    .use(serveStatic({ root: "public/" }));
 
   return app;
 };
