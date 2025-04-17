35569910325fdd69bd7c7d585ef268ef8ef9f199
Shubhabrata Ghosh <shubhabrata.g1108@gmail.com>
2025-04-17 12:47:47 +0530

    Update the deno.yml github action file

--- qa_setup.sh
+++ qa_setup.sh
@@ -8,8 +8,8 @@ deno install -gArf jsr:@deno/deployctl
 cat > deno.qa.json <<EOF
 {
   "tasks": {
-    "deploy:qa": "deployctl deploy --project=seven-wonders-qa --entrypoint=main.ts --root=.",
-    "deploy:prod": "deployctl deploy --project=seven-wonders-prod --entrypoint=main.ts --root=."
+    "deploy:qa": "deployctl deploy --project=seven-wonders-qa --entrypoint=main.js --root=.",
+    "deploy:prod": "deployctl deploy --project=seven-wonders-prod --entrypoint=main.js --root=."
   }
 }
 EOF
