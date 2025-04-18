841a579abd7ebedaddb7c2e4eab5ca4304b8a30c
Abhinav K <183721743+AbhinavKaippa@users.noreply.github.com>
2025-04-18 14:35:47 +0530

    [#22] Add test cases for wonder class

--- src/models/wonder.js
+++ src/models/wonder.js
@@ -1,123 +1,123 @@
 class Wonder {
-  wonder;
-  cards;
-  resources;
+  #wonder;
+  #cards;
+  #resources;
   // stages;
   // staged;
-  discounts;
-  militaryStrength;
-  victoryPoints;
+  #discounts;
+  #militaryStrength;
+  #victoryPoints;
 
-  constructor(wonder) {
-    this.wonder = wonder;
-    this.cards = {};
-    this.resources = { choices: [] }; // {ore: 2, choices: [[a,b], [a,b,c]]}
-    this.resources[wonder.resource] = 1;
+  constructor (wonder) {
+    this.#wonder = wonder;
+    this.#cards = {};
+    this.#resources = {choices: []}; // {ore: 2, choices: [[a,b], [a,b,c]]}
+    this.#resources[wonder.resource] = 1;
     // this.stages = wonder.stages;
     // this.staged = [];
-    this.discounts = {}; // {wood: [left]}
-    this.militaryStrength = 0;
-    this.victoryPoints = 0;
+    this.#discounts = {}; // {wood: [left]}
+    this.#militaryStrength = 0;
+    this.#victoryPoints = 0;
   }
 
-  isResourceCard(card) { //  incluse purple and yelow resource card also
+  isResourceCard (card) { //  incluse purple and yelow resource card also
     const resources = ["brown", "grey", "green"];
 
     return resources.includes(card.color);
   }
 
-  isDiscountCard(card) {
+  isDiscountCard (card) {
     const isYellowCard = card.color === "yellow";
     const isDiscountCard = card.effect && card.effect[0]?.effect_type === "buy";
 
     return isYellowCard && isDiscountCard;
   }
 
-  isMilitaryStrength(card) {
+  isMilitaryStrength (card) {
     return card.color == "red";
   }
 
-  isVictoryPoints(card) {
+  isVictoryPoints (card) {
     return card.color == "blue";
   }
 
-  addResources(card) {
-    if (card.produces[0].type === "choice") {
+  addResources (card) {
+    if(card.produces[0].type === "choice") {
       const options = card.produces[0].options;
-      this.resources.choices.push(new Set(options));
+      this.#resources.choices.push(new Set(options));
       return;
     }
 
     const resource = card.produces[0].type;
     const count = card.produces[0].count;
-    this.resources[resource] = this.resources[resource] || 0 + count;
+    this.#resources[resource] = (this.#resources[resource] || 0) + count;
   }
 
-  addDiscounts(card) {
+  addDiscounts (card) {
     const resources = card.effect[0].options;
     const newNeighbours = card.effect[0].applies_to;
 
     resources.forEach((resource) => {
-      const neighbours = this.discounts[resource];
+      const neighbours = this.#discounts[resource];
 
-      if (!neighbours) {
-        this.discounts[resource] = [];
+      if(!neighbours) {
+        this.#discounts[resource] = [];
       }
 
-      this.discounts[resource] = this.discounts[resource].concat(newNeighbours);
+      this.#discounts[resource] = this.#discounts[resource].concat(newNeighbours);
     });
   }
 
-  addVictoryPoints(card) {
+  addVictoryPoints (card) {
     const count = card.produces[0].count;
-    this.victoryPoints += count;
+    this.#victoryPoints += count;
   }
 
-  addMilitaryStrength(card) {
+  addMilitaryStrength (card) {
     const count = card.produces[0].count;
-    this.militaryStrength += count;
+    this.#militaryStrength += count;
   }
 
-  getCardBenifits(card) {
-    if (this.isResourceCard(card)) {
+  getCardBenifits (card) {
+    if(this.isResourceCard(card)) {
       this.addResources(card);
     }
 
-    if (this.isDiscountCard(card)) {
+    if(this.isDiscountCard(card)) {
       this.addDiscounts(card);
     }
 
-    if (this.isVictoryPoints(card)) {
+    if(this.isVictoryPoints(card)) {
       this.addVictoryPoints(card);
     }
 
-    if (this.isMilitaryStrength(card)) {
+    if(this.isMilitaryStrength(card)) {
       this.addMilitaryStrength(card);
     }
   }
 
-  build(card) {
-    this.cards[card.color] = card;
+  build (card) {
+    this.#cards[card.color] = card;
     this.getCardBenifits(card);
   }
 
   // stage (card) {}
 
-  get resources() {
-    return this.resources;
+  get resources () {
+    return this.#resources;
   }
 
-  get militaryStrength() {
-    return this.militaryStrength;
+  get militaryStrength () {
+    return this.#militaryStrength;
   }
 
-  get victoryPoints() {
-    return this.victoryPoints;
+  get victoryPoints () {
+    return this.#victoryPoints;
   }
 
-  get discounts() {
-    return this.discounts;
+  get discounts () {
+    return this.#discounts;
   }
 }
 
-export { Wonder };
+export {Wonder};
--- test/models_test.js
+++ test/models_test.js
@@ -1,8 +1,8 @@
-import { assert, assertEquals, assertNotEquals, assertThrows } from "assert";
-import { describe, it } from "test/bdd";
-import { Player } from "../src/models/player.js";
-import { Game } from "../src/models/game.js";
-import { Wonder } from "../src/models/wonder.js";
+import {assert, assertEquals, assertNotEquals, assertThrows} from "assert";
+import {describe, it} from "test/bdd";
+import {Player} from "../src/models/player.js";
+import {Game} from "../src/models/game.js";
+import {Wonder} from "../src/models/wonder.js";
 
 describe("Testing the Player class", () => {
   it("Two players' id should be not equal", () => {
@@ -58,22 +58,22 @@ describe("Testing the Game class", () => {
 
 describe("testing the wonders class", () => {
   const olympia = {
-    img: "olympiaA.jpeg",
-    name: "Olympia",
-    resource: "wood",
-    side: "A",
-    stages: {
-      stage1: {
-        resources: [{ type: "wood", count: 2 }],
-        powers: [{ type: "points", value: 3 }],
+    "img": "olympiaA.jpeg",
+    "name": "Olympia",
+    "resource": "wood",
+    "side": "A",
+    "stages": {
+      "stage1": {
+        "resources": [{"type": "wood", "count": 2}],
+        "powers": [{"type": "points", "value": 3}],
       },
-      stage2: {
-        resources: [{ type: "stone", count: 2 }],
-        powers: [{ type: "free_card_per_age" }],
+      "stage2": {
+        "resources": [{"type": "stone", "count": 2}],
+        "powers": [{"type": "free_card_per_age"}],
       },
-      stage3: {
-        resources: [{ type: "ore", count: 2 }],
-        powers: [{ type: "points", value: 7 }],
+      "stage3": {
+        "resources": [{"type": "ore", "count": 2}],
+        "powers": [{"type": "points", "value": 7}],
       },
     },
   };
@@ -82,39 +82,39 @@ describe("testing the wonders class", () => {
     const wonder = new Wonder(olympia);
 
     wonder.build({
-      name: "Guard Tower",
-      age: 1,
-      color: "red",
-      min_players: 3,
-      cost: [{ type: "clay", count: 1 }],
-      produces: [{ type: "shield", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Guard Tower",
+      "age": 1,
+      "color": "red",
+      "min_players": 3,
+      "cost": [{"type": "clay", "count": 1}],
+      "produces": [{"type": "shield", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Stockade",
-      age: 1,
-      color: "red",
-      min_players: 3,
-      cost: [{ type: "wood", count: 1 }],
-      produces: [{ type: "shield", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Stockade",
+      "age": 1,
+      "color": "red",
+      "min_players": 3,
+      "cost": [{"type": "wood", "count": 1}],
+      "produces": [{"type": "shield", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Barracks",
-      age: 1,
-      color: "red",
-      min_players: 3,
-      cost: [{ type: "ore", count: 1 }],
-      produces: [{ type: "shield", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: ["Stables", "Walls"],
+      "name": "Barracks",
+      "age": 1,
+      "color": "red",
+      "min_players": 3,
+      "cost": [{"type": "ore", "count": 1}],
+      "produces": [{"type": "shield", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": ["Stables", "Walls"],
     });
 
     const militaryStrength = wonder.militaryStrength;
@@ -126,51 +126,51 @@ describe("testing the wonders class", () => {
     const wonder = new Wonder(olympia);
 
     wonder.build({
-      name: "Altar",
-      age: 1,
-      color: "blue",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "points", count: 2 }],
-      chain_from: null,
-      chain_to: ["Temple"],
-      type: "civil",
+      "name": "Altar",
+      "age": 1,
+      "color": "blue",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "points", "count": 2}],
+      "chain_from": null,
+      "chain_to": ["Temple"],
+      "type": "civil",
     });
 
     wonder.build({
-      name: "Baths",
-      age: 1,
-      color: "blue",
-      min_players: 3,
-      cost: [{ type: "stone", count: 1 }],
-      produces: [{ type: "points", count: 3 }],
-      chain_from: null,
-      chain_to: ["Aqueduct"],
-      type: "civil",
+      "name": "Baths",
+      "age": 1,
+      "color": "blue",
+      "min_players": 3,
+      "cost": [{"type": "stone", "count": 1}],
+      "produces": [{"type": "points", "count": 3}],
+      "chain_from": null,
+      "chain_to": ["Aqueduct"],
+      "type": "civil",
     });
 
     wonder.build({
-      name: "Theater",
-      age: 1,
-      color: "blue",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "points", count: 2 }],
-      chain_from: null,
-      chain_to: ["Statue"],
-      type: "civil",
+      "name": "Theater",
+      "age": 1,
+      "color": "blue",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "points", "count": 2}],
+      "chain_from": null,
+      "chain_to": ["Statue"],
+      "type": "civil",
     });
 
     wonder.build({
-      name: "Pawn Shop",
-      age: 1,
-      color: "blue",
-      min_players: 4,
-      cost: [],
-      produces: [{ type: "points", count: 3 }],
-      chain_from: null,
-      chain_to: [],
-      type: "civil",
+      "name": "Pawn Shop",
+      "age": 1,
+      "color": "blue",
+      "min_players": 4,
+      "cost": [],
+      "produces": [{"type": "points", "count": 3}],
+      "chain_from": null,
+      "chain_to": [],
+      "type": "civil",
     });
 
     const victoryPoints = wonder.victoryPoints;
@@ -182,36 +182,36 @@ describe("testing the wonders class", () => {
     const wonder = new Wonder(olympia);
 
     wonder.build({
-      name: "Lumber Yard",
-      age: 1,
-      color: "brown",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "wood", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Lumber Yard",
+      "age": 1,
+      "color": "brown",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "wood", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Stone Pit",
-      age: 1,
-      color: "brown",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "stone", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Stone Pit",
+      "age": 1,
+      "color": "brown",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "stone", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Clay Pit",
-      age: 1,
-      color: "brown",
-      min_players: 3,
-      cost: [{ type: "coin", count: 1 }],
-      produces: [
+      "name": "Clay Pit",
+      "age": 1,
+      "color": "brown",
+      "min_players": 3,
+      "cost": [{"type": "coin", "count": 1}],
+      "produces": [
         {
           type: "choice",
           options: ["ore", "clay"],
@@ -242,75 +242,75 @@ describe("testing the wonders class", () => {
     });
 
     wonder.build({
-      name: "Glassworks",
-      age: 1,
-      color: "gray",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "glass", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Glassworks",
+      "age": 1,
+      "color": "gray",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "glass", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Press",
-      age: 1,
-      color: "gray",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "papyrus", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Press",
+      "age": 1,
+      "color": "gray",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "papyrus", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Loom",
-      age: 1,
-      color: "gray",
-      min_players: 3,
-      cost: [],
-      produces: [{ type: "textile", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: [],
+      "name": "Loom",
+      "age": 1,
+      "color": "gray",
+      "min_players": 3,
+      "cost": [],
+      "produces": [{"type": "textile", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": [],
     });
 
     wonder.build({
-      name: "Scriptorium",
-      age: 1,
-      color: "green",
-      min_players: 3,
-      cost: [{ type: "papyrus", count: 1 }],
-      produces: [{ type: "tablet", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: ["Library"],
+      "name": "Scriptorium",
+      "age": 1,
+      "color": "green",
+      "min_players": 3,
+      "cost": [{"type": "papyrus", "count": 1}],
+      "produces": [{"type": "tablet", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": ["Library"],
     });
 
     wonder.build({
-      name: "Apothecary",
-      age: 1,
-      color: "green",
-      min_players: 3,
-      cost: [{ type: "textile", count: 1 }],
-      produces: [{ type: "compass", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: ["Stables", "Dispensary"],
+      "name": "Apothecary",
+      "age": 1,
+      "color": "green",
+      "min_players": 3,
+      "cost": [{"type": "textile", "count": 1}],
+      "produces": [{"type": "compass", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": ["Stables", "Dispensary"],
     });
 
     wonder.build({
-      name: "Workshop",
-      age: 1,
-      color: "green",
-      min_players: 3,
-      cost: [{ type: "glass", count: 1 }],
-      produces: [{ type: "gear", count: 1 }],
-      effect: null,
-      chain_from: null,
-      chain_to: ["Laboratory"],
+      "name": "Workshop",
+      "age": 1,
+      "color": "green",
+      "min_players": 3,
+      "cost": [{"type": "glass", "count": 1}],
+      "produces": [{"type": "gear", "count": 1}],
+      "effect": null,
+      "chain_from": null,
+      "chain_to": ["Laboratory"],
     });
 
     const resources = wonder.resources;
@@ -323,7 +323,7 @@ describe("testing the wonders class", () => {
       gear: 1,
       stone: 1,
       tablet: 1,
-      wood: 1,
+      wood: 2,
     };
 
     assertEquals(resources, result);
@@ -341,11 +341,11 @@ describe("testing the wonders class", () => {
       produces: [],
       effect: [
         {
-          type: "resource",
-          effect_type: "buy",
-          cost: [{ type: "coin", count: 1 }],
-          applies_to: ["right_neighbour"],
-          options: ["clay", "stone", "wood", "ore"],
+          "type": "resource",
+          "effect_type": "buy",
+          "cost": [{"type": "coin", "count": 1}],
+          "applies_to": ["right_neighbour"],
+          "options": ["clay", "stone", "wood", "ore"],
         },
       ],
       chain_from: null,
@@ -361,11 +361,11 @@ describe("testing the wonders class", () => {
       produces: [],
       effect: [
         {
-          type: "resource",
-          effect_type: "buy",
-          cost: [{ type: "coin", count: 1 }],
-          applies_to: ["left_neighbour"],
-          options: ["clay", "stone", "wood", "ore"],
+          "type": "resource",
+          "effect_type": "buy",
+          "cost": [{"type": "coin", "count": 1}],
+          "applies_to": ["left_neighbour"],
+          "options": ["clay", "stone", "wood", "ore"],
         },
       ],
       chain_from: null,
@@ -381,11 +381,11 @@ describe("testing the wonders class", () => {
       produces: [],
       effect: [
         {
-          type: "resource",
-          effect_type: "buy",
-          cost: [{ type: "coin", count: 1 }],
-          applies_to: ["left_neighbour", "right_neighbour"],
-          options: ["glass", "papyrus", "textile"],
+          "type": "resource",
+          "effect_type": "buy",
+          "cost": [{"type": "coin", "count": 1}],
+          "applies_to": ["left_neighbour", "right_neighbour"],
+          "options": ["glass", "papyrus", "textile"],
         },
       ],
       chain_from: null,
