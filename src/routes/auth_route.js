c72d7165111e7b29643339cb1ab7c0eb51c4affd
Shubhabrata Ghosh <shubhabrata.g1108@gmail.com>
2025-04-18 13:29:47 +0530

    [#32] fix authentication bug | Shubhabrata If a player came with cookies, according to the cookie evaluate the login request

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -1,11 +1,32 @@
 import { Hono } from "hono";
 import { registerUser } from "../handlers/auth_handler.js";
+import { logger } from "hono/logger";
+
+const evaluateCookie = () => {
+  return async (ctx, next) => {
+    const playerID = ctx.getCookie(ctx, "playerID");
+    const gameID = ctx.getCookie(ctx, "gameID");
+    const playerGameMap = ctx.get("playerGameMap");
+
+    if (!playerID && !gameID) {
+      await next();
+    }
+    if (playerID && gameID) {
+      if (gameID === playerGameMap.get(playerID)) {
+        return ctx.redirect("/waiting_room.html");
+      }
+    }
+    return ctx.text("Access Denied!", 403);
+  };
+};
 
 const createAuthRoute = () => {
   const authApp = new Hono();
 
-  authApp.post("/login", registerUser);
-
+  authApp
+    .use(logger())
+    .use(evaluateCookie())
+    .post("/login", registerUser);
   return authApp;
 };
 
